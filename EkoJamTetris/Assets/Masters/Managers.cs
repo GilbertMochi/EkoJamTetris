1a3f2004ed5c875b3d641ed0eb1e11f7995065d8
Janessa <s00190968@mail.itsligo.ie>
2019-06-15 00:05:15 +0300

    main menu finished, no final graphics yet

---
+++ EkoJamTetris/Assets/Masters/Managers.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Managers : MonoBehaviour
+{
+    public static Managers Manager { get; private set; }
+
+    void Awake()
+    {
+        if (Manager == null)
+        {
+            Manager = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ EkoJamTetris/Assets/Masters/SceneMaster.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneMaster : MonoBehaviour
+{
+    private static bool gamePaused = false;
+
+    public static void OpenScene(int level)
+    {
+        SceneManager.LoadScene(level);
+    }
+
+    public static void OpenMainMenu()
+    {
+        gamePaused = false;
+        Time.timeScale = 1f;
+        SceneManager.LoadScene(0);
+    }
+
+    public static void PauseGame()
+    {
+        gamePaused = true;
+        Time.timeScale = 0f;//stops gametime
+    }
+
+    public static void ContinueGame()
+    {
+        gamePaused = false;
+        Time.timeScale = 1f; //resumes gametime
+    }
+
+    public static bool getIsGamePaused()
+    {
+        return gamePaused;
+    }
+}
