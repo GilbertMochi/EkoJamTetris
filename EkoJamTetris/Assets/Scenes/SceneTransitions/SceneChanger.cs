de8872fd113fa09151fa4233664c8b2dc8e564c7
Janessa <s00190968@mail.itsligo.ie>
2019-06-15 01:05:20 +0300

    scene transitions might work, I need a game level to test the fading out properly

---
+++ EkoJamTetris/Assets/Scenes/SceneTransitions/SceneChanger.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneChanger : MonoBehaviour
+{
+    public Animator anim;
+
+    int levelToLoad = 0;
+   
+    public void FadeToLevel(int level)
+    {
+        levelToLoad = level;
+        anim.SetTrigger("fadeOut");
+    }
+
+    public void onLevelFadeComplete()
+    {
+        SceneManager.LoadScene(levelToLoad);
+    }
+}
