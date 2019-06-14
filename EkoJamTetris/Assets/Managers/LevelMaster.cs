2ad563fc46fab9d221c32317e0994450fcb0d3b5
Janessa <s00190968@mail.itsligo.ie>
2019-06-14 23:16:59 +0300

    menu things straight into master sorry and you're welcome

---
+++ EkoJamTetris/Assets/Managers/LevelMaster.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelMaster : MonoBehaviour
+{
+    PlayerMovement playerMovement;
+    ObstacleSpawner obstacleSpawner;
+
+    public static int SecondsToAdd { get; set; } = 40;//seconds to be added to duration once game has lasted more than duration
+
+    private float elapsedSeconds;
+    public TimeSpan PlayTime//time the player has played the the game (gamescene)
+    {
+        get
+        {
+            return TimeSpan.FromSeconds(elapsedSeconds);//gets the passed time from elapsed seconds
+        }
+        private set
+        {
+            PlayTime = value;
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        elapsedSeconds = 0;//set the elapsed seconds to 0 at the start of the level
+
+        playerMovement = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerMovement>();
+        obstacleSpawner = GameObject.Find("obstacles").GetComponent<ObstacleSpawner>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!SceneMaster.getIsGamePaused())//if game is not paused (if game is running)
+        {
+            elapsedSeconds += Time.deltaTime;
+
+
+            if (PlayTime.TotalSeconds > SecondsToAdd && SecondsToAdd > 5)//everytime play time is longer than duration
+            {
+                //increase player speed
+                playerMovement.IncreaseSpeed(.4f);
+
+                if (obstacleSpawner.SpawnTime > 1.5)
+                {
+                    //spawn obstacles faster
+                    obstacleSpawner.SpawnTime -= .2f;
+                }
+
+                //decrease the time of the seconds to add
+                SecondsToAdd -= 2;
+            }
+        }
+    }
+
+}
---
+++ EkoJamTetris/Assets/Managers/ScoreMaster.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScoreMaster : MonoBehaviour
+{
+    public static int PlayScore { get; private set; }
+    public int ScoreAmountEverySecond = 1;//amount of score added to player score every second
+    LevelMaster levMas;
+
+    TimeSpan duration;
+    float lastUpdate;
+
+    public void ReduceFromPlayScore(int amount)
+    {
+        PlayScore -= amount;
+    }
+
+    private void Start()
+    {
+        levMas = GetComponent<LevelMaster>();
+        duration = new TimeSpan(0, 0, LevelMaster.SecondsToAdd);
+        PlayScore = 0;
+    }
+
+    private void Update()
+    {
+        if (Time.time - lastUpdate >= 1f)//if a second has passed since lastUpdate
+        {
+            PlayScore += ScoreAmountEverySecond;//adds to player score every second
+            lastUpdate = Time.time;//last update is time now
+        }
+
+        if (levMas.PlayTime.TotalSeconds > duration.TotalSeconds)//everytime play time is longer than duration
+        {
+            PlayScore += (int)duration.TotalSeconds;//add the amount of seconds in duration to playerscore
+            //Debug.Log($"Play score: {PlayScore}");
+
+            duration += new TimeSpan(0, 0, LevelMaster.SecondsToAdd);//increase duration
+            //Debug.Log($"New duration is {duration.ToString()}");
+
+            ScoreAmountEverySecond++;
+        }
+    }
+}
