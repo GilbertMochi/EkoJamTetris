8c0cca395b43bdd84623266bf10b1fa4fd83b834
Janessa <s00190968@mail.itsligo.ie>
2019-06-15 01:15:13 +0300

    added fadeout completed actions

--- EkoJamTetris/Assets/Scenes/SceneTransitions/SceneChanger.cs
+++ EkoJamTetris/Assets/Scenes/SceneTransitions/SceneChanger.cs
@@ -5,17 +5,26 @@ using UnityEngine.SceneManagement;
 
 public class SceneChanger : MonoBehaviour
 {
-    public Animator anim;
+    Animator anim;
 
     int levelToLoad = 0;
-   
+
+    private void Start()
+    {
+        anim = GetComponent<Animator>();
+        if(anim == null)
+        {
+            Debug.LogError("No animator in" + transform.name);
+        }
+    }
+
     public void FadeToLevel(int level)
     {
         levelToLoad = level;
         anim.SetTrigger("fadeOut");
     }
 
-    public void onLevelFadeComplete()
+    public void OnLevelFadeComplete()
     {
         SceneManager.LoadScene(levelToLoad);
     }
