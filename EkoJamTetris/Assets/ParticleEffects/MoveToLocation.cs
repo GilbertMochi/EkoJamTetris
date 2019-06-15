df3bb832f8e78945678f9e7f8d22c0567a72aefa
fredu85 <fredrik.lassila85@hotmail.com>
2019-06-15 15:53:07 +0300

    Effectzzzzz

---
+++ EkoJamTetris/Assets/ParticleEffects/MoveToLocation.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveToLocation : MonoBehaviour
+{
+    public ParticleSystem p;
+    public ParticleSystem.Particle[] particles;
+    public GameObject Targetobject;
+    public float affectDistance;
+    float sqrDist;
+    Transform thisTransform;
+    Transform Target;
+
+
+    void Start()
+    {
+        p = GetComponent<ParticleSystem>();
+        sqrDist = affectDistance * affectDistance;
+    }
+
+
+    void Update()
+    {
+        Target = Targetobject.transform;
+        particles = new ParticleSystem.Particle[p.particleCount];
+
+        p.GetParticles(particles);
+
+        for (int i = 0; i < particles.GetUpperBound(0); i++)
+        {
+           // Vector3 pos = new Vector3(particles[i].position.x, particles[i].position.y, particles[i].position.z);
+           // Vector3 pos2 = new Vector3(Target.position.x, Target.position.y, Target.position.z);
+
+            //if (Vector3.Distance(Target.position, particles[i].position)<2f )
+            //    return;
+
+            float ForceToAdd = (particles[i].startLifetime - particles[i].remainingLifetime) * (0.5f * Vector3.Distance(Target.position, particles[i].position));
+
+            Debug.DrawRay(particles[i].position, (Target.position - particles[i].position).normalized * (ForceToAdd / 10));
+            Vector3 direction = new Vector2(Target.position.x - particles[i].position.x, Target.position.y - particles[i].position.y);
+            particles[i].velocity = direction.normalized * ForceToAdd;
+            //(Target.position - particles[i].position)
+             // particles[i].position = Vector3.Lerp (particles [i].position, Target.position, Time.deltaTime / 1f);
+
+        }
+
+        p.SetParticles(particles, particles.Length);
+
+    }
+}
---
+++ EkoJamTetris/Assets/SetSpawnPoint.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SetSpawnPoint : MonoBehaviour
+{
+    public List<GameObject> SpawnLocations;
+
+    [SerializeField]
+    GameObject _SpawnLocation;
+    [SerializeField]
+    ParticleSystem SmallCloudSpawner;
+    [SerializeField]
+    ParticleSystem InitialCloudSpawner;
+
+    int _Index;
+
+    private void Start()
+    {
+        _Index = 0;
+        if (SpawnLocations.Count < 1)
+            return;
+
+        _SpawnLocation.transform.position = SpawnLocations[_Index].transform.position;
+        InitialCloudSpawner.transform.position = _SpawnLocation.transform.position;
+        SmallCloudSpawner.Play();
+        InitialCloudSpawner.Play();
+        StartCoroutine(GoToHere());
+
+    }
+
+    private void FixedUpdate()
+    {
+        if (SpawnLocations.Count < 1)
+            return;
+        if (Vector3.Distance(SpawnLocations[_Index].transform.position, _SpawnLocation.transform.position) < 0.2)
+            return;
+        if (_Index > 1 && _Index < SpawnLocations.Count)
+            _SpawnLocation.transform.position = Vector3.Lerp
+                    (SpawnLocations[_Index - 1].transform.position, SpawnLocations[_Index].transform.position, Time.deltaTime / 50f);
+    }
+
+    IEnumerator GoToHere()
+    { 
+            SmallCloudSpawner.Play();
+            _SpawnLocation.transform.position = SpawnLocations[_Index].transform.position;
+            yield return new WaitForSeconds(4f);
+            InitialCloudSpawner.Stop();
+            SpawnLocations[_Index].SetActive(true);
+
+        if (_Index + 1 < SpawnLocations.Count)
+        {
+            _Index++;
+            StartCoroutine(GoToHere());
+        }
+        else
+        {
+            SmallCloudSpawner.Stop();
+            InitialCloudSpawner.Stop();
+        }
+ 
+    }
+}
