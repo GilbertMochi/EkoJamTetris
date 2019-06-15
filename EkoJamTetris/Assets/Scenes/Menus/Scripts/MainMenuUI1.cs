f3367b16fb0ef1186f9a0521e15d3de824dfaacc
Janessa <s00190968@mail.itsligo.ie>
2019-06-15 17:00:50 +0300

    credits page added to main menu

--- EkoJamTetris/Assets/Scenes/Menus/Scripts/MainMenuUI1.cs
+++ EkoJamTetris/Assets/Scenes/Menus/Scripts/MainMenuUI1.cs
@@ -7,6 +7,7 @@ public class MainMenuUI1 : MonoBehaviour
     public GameObject SettingsPanel;
     public GameObject GameHelpPanel;
     public GameObject MainMenuPanel;
+    public GameObject CreditsPanel;
 
     // Start is called before the first frame update
     void Start()
@@ -14,6 +15,7 @@ public class MainMenuUI1 : MonoBehaviour
         //make sure the panels are closed at the start but the buttons are active
         SettingsPanel.SetActive(false);
         GameHelpPanel.SetActive(false);
+        CreditsPanel.SetActive(false);
         MainMenuPanel.SetActive(true);//this panel has the buttons
     }
 
@@ -34,10 +36,17 @@ public class MainMenuUI1 : MonoBehaviour
         MainMenuPanel.SetActive(false);
     }
 
+    public void OpenCredits()
+    {
+        CreditsPanel.SetActive(true);
+        MainMenuPanel.SetActive(false);
+    }
+
     public void CloseAllPanels()
     {
         SettingsPanel.SetActive(false);//close settings
         GameHelpPanel.SetActive(false);//close game help
+        CreditsPanel.SetActive(false);//close game help
         MainMenuPanel.SetActive(true);//opens the panel with the buttons
     }
 
