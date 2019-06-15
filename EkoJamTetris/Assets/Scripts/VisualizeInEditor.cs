91b0f899f08e990dd97959c35963bd81f00cbebc
Teemu Turku <teemu.turku@pp2.inet.fi>
2019-06-16 01:56:23 +0300

    level1 alku

---
+++ EkoJamTetris/Assets/Scripts/VisualizeInEditor.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class VisualizeInEditor : MonoBehaviour
+{
+    public bool drawInEditor;
+
+    private void OnDrawGizmos() {
+        if(drawInEditor)
+        {
+            Gizmos.color = Color.black;
+            Gizmos.DrawSphere(transform.position, 0.4f);
+        }
+    }
+}
