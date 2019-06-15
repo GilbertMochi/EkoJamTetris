9cd7f9a18c9d13a0a4afdf68f4792ea93b8518c6
Janessa <s00190968@mail.itsligo.ie>
2019-06-15 22:24:25 +0300

    game level ui, house spawning in correct positions

--- EkoJamTetris/Assets/Scripts/MovingBGObjects/SimpleCar.cs
+++ EkoJamTetris/Assets/Scripts/MovingBGObjects/SimpleCar.cs
@@ -3,20 +3,43 @@ using System.Collections.Generic;
 using UnityEngine;
 
 [RequireComponent(typeof(Rigidbody2D))]//has to have a rigidbody2d attached
-public class SimpleCar : MonoBehaviour, IPooledObject
+public class SimpleCar : Sprite, IPooledObject
 {
     Rigidbody2D rb;
 
     [SerializeField]
     float forceX = 6f;
+
+    float distanceFromLeftSpawnPoint, distanceFromRightSpawnPoint;
+    Transform leftSpawnPoint, rightSpawnPoint;
+
     void Awake()
     {
         rb = GetComponent<Rigidbody2D>();//get rigidbody from self
     }
+
     public void OnObjectSpawn()//do this everytime object is spawned from the pool
     {
         //Debug.Log(transform.name + " on object spawn called in simple car script.");
-        rb.velocity = new Vector2(forceX, 0);
-    }
+        
+        //get the car's right and left spawn points from the object spawner
+        leftSpawnPoint =  GameObject.Find("carLeftSpawnPoint").transform;
+        rightSpawnPoint = GameObject.Find("carRightSpawnPoint").transform;
 
+        //calculate distances from each spawn point
+        distanceFromLeftSpawnPoint = Vector3.Distance(transform.position, leftSpawnPoint.position);
+        distanceFromRightSpawnPoint = Vector3.Distance(transform.position, rightSpawnPoint.position);
+
+        //car was spawned to right, move left
+        if (distanceFromLeftSpawnPoint > distanceFromRightSpawnPoint)
+        {
+            rb.velocity = new Vector2(-forceX, 0);
+        }
+
+        //car was spawned to left, move right
+        if (distanceFromLeftSpawnPoint < distanceFromRightSpawnPoint)
+        {
+            rb.velocity = new Vector2(forceX, 0);
+        }
+    }
 }
---
+++ EkoJamTetris/Assets/Scripts/MovingBGObjects/Sprite.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sprite : MonoBehaviour
+{
+
+    //sprite variables
+    bool facingRight = true;
+    float lastPosX;
+    Vector3 startScale;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        startScale = transform.localScale;//scale at the start
+        lastPosX = transform.position.x;//last position
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //change the direction the sprite is facing
+        if (lastPosX < transform.position.x && !facingRight)//moves right but isn't facing right
+        {
+            facingRight = true;
+            flipSprite();
+        }
+        if (lastPosX > transform.position.x && facingRight)//moves left but isn't facing left
+        {
+            facingRight = false;
+            flipSprite();
+        }
+    }
+
+    //scale the object so that it's facing the other direction
+    public void flipSprite()
+    {
+        if (!facingRight)
+        {
+            transform.localScale = new Vector3(-startScale.x, startScale.y, startScale.z);
+        }
+        else
+        {
+            transform.localScale = new Vector3(startScale.x, startScale.y, startScale.z);
+        }
+    }
+}
